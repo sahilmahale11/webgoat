3eb26effaa0ddd2e02901c5356cedc4adb62dc73
root <root@ip-172-31-35-236.ec2.internal>
2024-10-07 06:03:36 +0000

    script5

--- sonarqube_report.sh
+++ sonarqube_report.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -u squ_de715a18ac0c010239d5fbb220e42fca00c1a21f: "http://54.210.41.141:9000/api/issues/search?projectKeys=org.owasp.webgoat:webgoat&p=1&ps=500" -o sonarqube_report.json
+curl -u squ_de715a18ac0c010239d5fbb220e42fca00c1a21f: "http://18.206.146.43:9000/api/issues/search?projectKeys=org.owasp.webgoat:webgoat&p=1&ps=500" -o sonarqube_report.json
 
 python3 convert_json_to_the_csv.py
